b640fd319d9e7ccef15645fa37d230353192dd5c
Mohamad-Mahdi Tasha <imwhdiiii@gmail.com>
2025-07-12 09:18:18 +0330

    added layouts and manifest.json

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,59 @@
 // Codes by mahdi tasha
 // Importing part
 import '@/style/index.css';
+import { RootLayoutProps } from '@/type/component';
+import { ReactNode } from 'react';
+import { Open_Sans } from 'next/font/google';
+import { cn } from '@/lib/utils';
+import type { Metadata } from 'next';
+
+// Defining metadata of the pages
+export const metadata: Metadata = {
+  title: 'Chand — Modern Web & PWA App',
+  description:
+    'Chand is a fast, modern web and PWA version of the original iOS app — rebuilt with custom UI and enhanced performance.',
+  applicationName: 'Chand',
+  keywords: [
+    'Chand',
+    'PWA',
+    'Modern App',
+    'Web App',
+    'Chand iOS',
+    'Next.js',
+    'React',
+    'Mahdi Tasha',
+  ],
+  creator: 'Mahdi Tasha',
+  manifest: '/manifest.json',
+  viewport: 'width=device-width, initial-scale=1, maximum-scale=1',
+  robots: {
+    index: true,
+    follow: true,
+    nocache: false,
+  },
+};
+
+// Defining font
+const OpenSans = Open_Sans({
+  display: 'block',
+  style: ['normal'],
+  subsets: ['latin'],
+  weight: ['300', '500', '600', '800'],
+});
+
+// Creating and exporting RootLayout component as default
+export default function RootLayout({ children }: RootLayoutProps): ReactNode {
+  // Returning JSX
+  return (
+    <html suppressHydrationWarning>
+      <body
+        className={cn(
+          'bg-background min-h-screen overflow-x-hidden overflow-y-auto text-foreground',
+          OpenSans.className,
+        )}
+      >
+        {children}
+      </body>
+    </html>
+  );
+}
