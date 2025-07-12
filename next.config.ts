9faee82d2806afaec56eccbef38f5c0925d0f300
Mohamad-Mahdi Tasha <imwhdiiii@gmail.com>
2025-07-12 08:14:56 +0330

    added comments of next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
-import type { NextConfig } from "next";
+// Codes by mahdi tasha
+// Importing part
+import type { NextConfig } from 'next';
 
-const nextConfig: NextConfig = {
-  /* config options here */
-};
+// Defining configs of next.js
+const nextConfig: NextConfig = {};
 
+// Exporting the configs as default
 export default nextConfig;
